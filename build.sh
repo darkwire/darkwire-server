184c6eb26559628a7c4694ac9ac3bb788eb7ad51
Alan Friedman <d.alan.friedman@gmail.com>
2019-05-11 22:32:15 -0400

    Try cloning client

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
-rm -rf build
-rm -rf dist
-babel src -d dist/src --copy-files
+echo "running build script"
 
-version=`git describe --abbrev=0 --tags`
-sha=`git rev-parse HEAD`
+git clone https://github.com/darkwire/darkwire-client.git
 
-echo $version
-echo $sha
-
-perl -pi -e "s/SHA/$sha/g" dist/src/config.json
-perl -pi -e "s/VERSION/$version/g" dist/src/config.json
-
-mkdir build
-zip -r build/build.zip . -x \"src\" -x \".git\"
+yarn build
