f4207faa7fff45edfd4483025bfc5be263ae6567
Alan Friedman <d.alan.friedman@gmail.com>
2019-05-12 15:44:36 -0400

    Cleanup scripts and readme

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@ rm -rf build
 rm -rf dist
 npx babel src -d dist/src --copy-files
 
-# version=`git describe --abbrev=0 --tags`
-# sha=`git rev-parse HEAD`
+sha=`git rev-parse HEAD`
 
-# echo $version
-# echo $sha
+echo $sha
 
-# perl -pi -e "s/SHA/$sha/g" dist/src/config.json
-# perl -pi -e "s/VERSION/$version/g" dist/src/config.json
+perl -pi -e "s/SHA/$sha/g" dist/src/config.json
