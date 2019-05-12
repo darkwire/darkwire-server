e2da904ecb91bddc10b780e48ce19567e5601d98
Alan Friedman <d.alan.friedman@gmail.com>
2019-05-11 23:10:51 -0400

    Build server code

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-echo "running build script"
-
+echo "building client..."
 git clone https://github.com/darkwire/darkwire-client.git
-
 cd darkwire-client
-
 yarn
-
 SKIP_PREFLIGHT_CHECK=true yarn react-scripts build
 
+echo "building server..."
 cd ../
+rm -rf build
+rm -rf dist
+babel src -d dist/src --copy-files
